6d6daf024d7631fb75fd04372ea91fc1c633ada1
Souler <soulerhyd@gmail.com>
2011-11-17 16:37:18 +0100

    Scripts/ToC5: Another try for fixing crashes on ghouls explode in phase change

--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheChampion/boss_black_knight.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheChampion/boss_black_knight.cpp
@@ -304,8 +304,9 @@ public:
                 return;
 
             for (SummonList::iterator itr = summons.begin(); itr != summons.end(); ++itr)
-                if (Creature* ghoul = me->GetCreature(*me, *itr))
-                    ghoul->CastSpell(ghoul, SPELL_EXPLODE, true);
+                if (Unit* unit = ObjectAccessor::GetUnit(*me, *itr))
+                    if (Creature* ghoul = unit->ToCreature())
+                        ghoul->CastSpell(ghoul, SPELL_EXPLODE);
         }
 
         void JustDied(Unit* /*killer*/)
